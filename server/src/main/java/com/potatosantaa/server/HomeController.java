0a581a759455a1a5b580a9e547b3307f5f9fa134
ArturoP <arturo_pan99@hotmail.com>
2020-09-21 13:37:31 -0700

    first commit

---
+++ server/src/main/java/com/potatosantaa/server/HomeController.java
@@ -0,0 +1,19 @@
+package com.potatosantaa.server;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/api")
+public class HomeController {
+    
+    String str = "Testing controller";
+
+    @GetMapping("/home")
+    public String getHome() {
+        return this.str;
+    }    
+}
